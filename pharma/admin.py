7452fe0180d52c26badd0efc92cbac5aa7cf0102
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2024-04-19 11:46:32 +0000

    telegram

--- pharma/admin.py
+++ pharma/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import *
+from .telegram_models import *
 
 
 class ConfigAdmin(admin.ModelAdmin):
     list_display = ('key', 'value')
 
+class TelegramUserChatAdmin(admin.ModelAdmin):
+    list_display = ('code', 'patient', 'telegram_chat_id', 'confirmed')
+
 admin.site.register(Config, ConfigAdmin)
+admin.site.register(TelegramUserChat, TelegramUserChatAdmin)
