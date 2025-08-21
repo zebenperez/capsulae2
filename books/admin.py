d9a7323cff490d2cb6476881d11ca021bb4c308e
Zeben Perez <zeben@vps-d65b23f4.vps.ovh.net>
2025-08-21 10:42:21 +0000

    ajuste libros

--- books/admin.py
+++ books/admin.py
@@ -3,4 +3,5 @@ from .models import *
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('isbn', 'title', 'subtitle', 'authors', 'serie', 'publisher_code', 'publisher_name')
+    search_fields = ('isbn',)
 admin.site.register(Book, BookAdmin)
